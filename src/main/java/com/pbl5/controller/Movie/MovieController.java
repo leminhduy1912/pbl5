5d0bac23a5286ba42751b4b55f3557eefb1bf1f0
leminhduy1912 <minhduyle081003@gmail.com>
2024-01-28 18:55:18 +0700

    add filer for admin

--- src/main/java/com/pbl5/controller/Movie/MovieController.java
+++ src/main/java/com/pbl5/controller/Movie/MovieController.java
@@ -1,11 +1,26 @@
 package com.pbl5.controller.Movie;
 
+import com.pbl5.dtos.UserSignInDTO;
+import com.pbl5.helpers.Http;
+import com.pbl5.utils.exceptions.ErrorHandler;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 import static com.pbl5.utils.constants.Endpoint.*;
 
 @WebServlet(urlPatterns = {V1  +ADMIN+ "/movie"})
 @MultipartConfig
-public class MovieController {
+public class MovieController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("test");
+    }
 }
--- src/main/java/com/pbl5/filter/AdminFilter.java
+++ src/main/java/com/pbl5/filter/AdminFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {Endpoint.V1 + Endpoint.PRIVATE + Endpoint.ADMIN + "/*"})
+@WebFilter(urlPatterns = {Endpoint.V1 + Endpoint.ADMIN + "/*"})
 public class AdminFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
