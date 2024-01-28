e6981dfd44fd2dcda3ff98ea14e38f4dc8574c89
leminhduy1912 <minhduyle081003@gmail.com>
2024-01-28 18:57:23 +0700

    add filter for admin

--- src/main/java/com/pbl5/controller/Movie/MovieController.java
+++ src/main/java/com/pbl5/controller/Movie/MovieController.java
@@ -21,6 +21,6 @@ public class MovieController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().write("test");
+        resp.getWriter().write("test thu");
     }
 }
